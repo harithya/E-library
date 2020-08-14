69ff6ec0b6cfd3010a85b5067e9d0f5e262199fc
Harithya <harithya77@gmail.com>
2020-08-14 10:36:48 +0700

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 134,
+    nav: false,
+    dot: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+});
